a17eef233ac6288d1b15b3494219d952a2537c64
Callum Mason <callum@mason.co.uk>
2021-01-15 15:47:43 +0000

    Added a JSON view

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,93 @@
 <template>
-  <v-container class="grey lighten-5">
-    <br />
-    <div class="Random All">
-      <h2>Randomise All</h2>
-      <v-btn
-        v-on:click="
-          RandomDisadvantage();
-          RandomExoticArmor('ALL');
-          RandomWeponType();
-          RandomExoticWepon();
-        "
-        elevation="7"
-        rounded
-        >Randomise All</v-btn
-      >
-    </div>
-    <br />
-    <br />
-    <br />
-    <br />
-    <div class="Disadvantage">
-      <h2>Random Disadvantage</h2>
-      <v-btn v-on:click="RandomDisadvantage" elevation="7" rounded
-        >Random Disadvantage</v-btn
-      >
-      <h3>Name : {{ DisadvantageName }}</h3>
-      <h3>Description : {{ DisadvantageDesc }}</h3>
-    </div>
-    <br />
-    <div class="Exotic">
-      <h2>Random Exotic Armor</h2>
-      <v-btn v-on:click="RandomExoticArmor('ALL')" elevation="7" rounded
-        >Random All Classes</v-btn
-      >
-      <v-btn v-on:click="RandomExoticArmor('Hunter')" elevation="7" rounded
-        >Random Hunter</v-btn
-      >
-      <v-btn v-on:click="RandomExoticArmor('Warlock')" elevation="7" rounded
-        >Random Warlock</v-btn
-      >
-      <v-btn v-on:click="RandomExoticArmor('Titan')" elevation="7" rounded
-        >Random Titan</v-btn
-      >
-      <h3>Class: {{ HuntTitWar }}</h3>
-      <h3>Exotic Armor: {{ ExoticArmor }}</h3>
-    </div>
-    <br />
-    <div class="Random Wepon Type">
-      <h2>Random Wepon Type</h2>
-      <v-btn v-on:click="RandomWeponType" elevation="7" rounded
-        >Random Wepon Type</v-btn
-      >
-      <h3>Energy : {{ WeponTypeEnergy }}</h3>
-      <h3>Kinetic : {{ WeponTypeKinetic }}</h3>
-      <h3>Power: {{ WeponTypePower }}</h3>
-    </div>
-    <br />
-    <div class="Random Exotic Wepon">
-      <h2>Random Exotic Wepon</h2>
-      <v-btn v-on:click="RandomExoticWepon" elevation="7" rounded
-        >Random Exotic Wepon</v-btn
-      >
-      <h3>Wepon : {{ ExoticWepon }}</h3>
-    </div>
-  </v-container>
+  <v-app>
+    <v-navigation-drawer width="75%" v-model="drawer" right temporary app>
+      <h1>JSON View</h1>
+
+      <div>
+        <tree-view :data="JSONDATA" :options="{ maxDepth: 4 }"></tree-view>
+      </div>
+
+      <!-- -->
+    </v-navigation-drawer>
+    <v-main>
+      <v-container fluid>
+        <br />
+        <div class="Random All">
+          <h2>Randomise All</h2>
+          <v-btn
+            v-on:click="
+              RandomDisadvantage();
+              RandomExoticArmor('ALL');
+              RandomWeponType();
+              RandomExoticWepon();
+            "
+            elevation="7"
+            rounded
+            ><v-icon>mdi-autorenew</v-icon>Randomise All</v-btn
+          >
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <div class="Disadvantage">
+          <h2>Random Disadvantage</h2>
+          <v-btn v-on:click="RandomDisadvantage" elevation="7" rounded
+            ><v-icon>mdi-autorenew</v-icon>Random Disadvantage</v-btn
+          >
+          <h3>Name : {{ DisadvantageName }}</h3>
+          <h3>Description : {{ DisadvantageDesc }}</h3>
+        </div>
+        <br />
+        <div class="Exotic">
+          <h2>Random Exotic Armor</h2>
+          <v-btn v-on:click="RandomExoticArmor('ALL')" elevation="7" rounded
+            ><v-icon>mdi-autorenew</v-icon>Random All Classes</v-btn
+          ><br />
+          <v-btn v-on:click="RandomExoticArmor('Hunter')" elevation="7" rounded
+            ><v-icon>mdi-autorenew</v-icon>Random Hunter</v-btn
+          ><br />
+          <v-btn v-on:click="RandomExoticArmor('Warlock')" elevation="7" rounded
+            ><v-icon>mdi-autorenew</v-icon>Random Warlock</v-btn
+          ><br />
+          <v-btn v-on:click="RandomExoticArmor('Titan')" elevation="7" rounded
+            ><v-icon>mdi-autorenew</v-icon>Random Titan</v-btn
+          >
+          <h3>Class: {{ HuntTitWar }}</h3>
+          <h3>Exotic Armor: {{ ExoticArmor }}</h3>
+        </div>
+        <br />
+        <div class="Random Wepon Type">
+          <h2>Random Wepon Type</h2>
+          <v-btn v-on:click="RandomWeponType" elevation="7" rounded
+            ><v-icon>mdi-autorenew</v-icon>Random Wepon Type</v-btn
+          >
+          <h3>Energy : {{ WeponTypeEnergy }}</h3>
+          <h3>Kinetic : {{ WeponTypeKinetic }}</h3>
+          <h3>Power: {{ WeponTypePower }}</h3>
+        </div>
+        <br />
+        <div class="Random Exotic Wepon">
+          <h2>Random Exotic Wepon</h2>
+          <v-btn v-on:click="RandomExoticWepon" elevation="7" rounded
+            ><v-icon>mdi-autorenew</v-icon>Random Exotic Wepon</v-btn
+          >
+          <h3>Wepon : {{ ExoticWepon }}</h3>
+        </div>
+      </v-container>
+    </v-main>
+    <v-footer app>
+      <v-btn elevation="7" rounded @click.stop="drawer = !drawer">View JSON</v-btn>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import jsondata from "../public/Data/Data.json";
+import TreeView from "vue-json-tree-view";
+Vue.use(TreeView);
 // import HelloWorld from "./components/HelloWorld.vue";
 
 function getRandomInt(max: number) {
@@ -91,7 +110,15 @@ export default Vue.extend({
       WeponTypeKinetic: null,
       WeponTypePower: null,
       ExoticWepon: null,
+      JSONDATA: jsondata,
+      drawer: false,
+      group: null,
     };
+  },
+  watch: {
+    group() {
+      this.drawer = false;
+    },
   },
 
   methods: {
