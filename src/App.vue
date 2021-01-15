404f882462847e419de6f67412b67132abeff20e
Callum Luke Mason <clmason99@hotmail.co.uk>
2021-01-15 22:16:10 +0000

    ESLINT and sorted checkboxes

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,76 @@
 <template>
   <v-app>
     <v-navigation-drawer width="75%" v-model="drawer" right temporary app style="padding-left: 2.5%">
-      <navcomp/>
+      <navcomp />
     </v-navigation-drawer>
     <v-main>
       <v-container fluid>
         <br />
+
+
         <div class="Random All">
-          <h2>Randomise All</h2>
-          <v-btn v-on:click=" RandomDisadvantage(); RandomExoticArmor('ALL'); RandomWeaponType(); RandomExoticWeapon(); " elevation="7" rounded><v-icon>mdi-autorenew</v-icon>Randomise All</v-btn>
+          <h2><v-btn v-on:click=" RandomDisadvantage(); RandomExoticArmor(); RandomWeaponType(); RandomExoticWeapon(); " elevation="7" rounded> <v-icon>mdi-autorenew</v-icon></v-btn> Randomise All</h2>
         </div>
+
+
         <br />
+
+
         <div class="Disadvantage">
-          <h2>Random Disadvantage</h2>
-          <v-btn v-on:click="RandomDisadvantage" elevation="7" rounded>
-            <v-icon>mdi-autorenew</v-icon>Random Disadvantage
-          </v-btn>
+          <h2><v-btn v-on:click="RandomDisadvantage" elevation="7" rounded><v-icon>mdi-autorenew</v-icon></v-btn>Random Disadvantage</h2>
           <h3>Name : {{ DisadvantageName }}</h3>
           <h3>Description : {{ DisadvantageDesc }}</h3>
         </div>
+
+
         <br />
+
+
         <div class="Exotic">
-          <h2>Random Exotic Armor</h2>
-          <v-checkbox v-model="ClassCheckBox" label="Hunter" value="Hunter" ></v-checkbox>
-          <v-checkbox v-model="ClassCheckBox" label="Warlock" value="Warlock" ></v-checkbox>
-          <v-checkbox v-model="ClassCheckBox" label="Titan" value="Titan" ></v-checkbox>
-          <v-btn v-on:click="RandomExoticArmor('ALL')" elevation="7" rounded> <v-icon>mdi-autorenew</v-icon>Random All Classes </v-btn><br />
+          <h2><v-btn v-on:click="RandomExoticArmor()" elevation="7" rounded><v-icon>mdi-autorenew</v-icon></v-btn> Random Exotic Armor</h2>
+          <v-container>
+            <v-row>
+              <v-col>
+                  <v-checkbox v-model="ClassCheckBox" label="Hunter" value="Hunter">
+                  </v-checkbox>
+              </v-col>
+              <v-col>
+                  <v-checkbox v-model="ClassCheckBox" label="Warlock" value="Warlock">
+                  </v-checkbox>
+              </v-col>
+              <v-col>
+                  <v-checkbox v-model="ClassCheckBox" label="Titan" value="Titan">
+                  </v-checkbox>
+              </v-col>
+              <v-spacer></v-spacer>
+              <v-spacer></v-spacer>
+              <v-spacer></v-spacer>
+            </v-row>
+          </v-container>
           <h3>Class: {{ HuntTitWar }}</h3>
           <h3>Exotic Armor: {{ ExoticArmor }}</h3>
         </div>
         <br />
         <div class="Random Weapon Type">
-          <h2>Random Weapon Type</h2>
-          <v-btn v-on:click="RandomWeaponType" elevation="7" rounded>
-            <v-icon>mdi-autorenew</v-icon>Random Weapon Type
-          </v-btn>
+          
+          <h2><v-btn v-on:click="RandomWeaponType" elevation="7" rounded><v-icon>mdi-autorenew</v-icon></v-btn>Random Weapon Type</h2>
+          <br>
           <h3>Energy : {{ WeaponTypeEnergy }}</h3>
           <h3>Kinetic : {{ WeaponTypeKinetic }}</h3>
           <h3>Power: {{ WeaponTypePower }}</h3>
         </div>
         <br />
         <div class="Random Exotic Weapon">
-          <h2>Random Exotic Weapon</h2>
-          <v-btn v-on:click="RandomExoticWeapon" elevation="7" rounded>
-            <v-icon>mdi-autorenew</v-icon>Random Exotic Weapon
-          </v-btn>
+          <h2><v-btn v-on:click="RandomExoticWeapon" elevation="7" rounded><v-icon>mdi-autorenew</v-icon></v-btn>Random Exotic Weapon</h2>
+          <br>
           <h3>Weapon : {{ ExoticWeapon }}</h3>
         </div>
       </v-container>
     </v-main>
     <v-footer app>
-      <v-btn elevation="7" rounded @click.stop="drawer = !drawer"
-        >View JSON</v-btn>
+      <v-btn elevation="7" rounded @click.stop="drawer = !drawer">
+        View JSON
+      </v-btn>
       <!-- <v-btn on:click="changeLocale()" elevation="7" rounded>Change Locale</v-btn> -->
       <!-- -->
     </v-footer>
@@ -61,7 +80,7 @@
 <script lang="ts">
 import Vue from "vue";
 import jsondata from "../public/Data/Data.json";
-import navcomp from "./components/nav.vue"
+import navcomp from "./components/nav.vue";
 // import HelloWorld from "./components/HelloWorld.vue";
 
 function getRandomInt(max: number) {
@@ -72,7 +91,7 @@ export default Vue.extend({
   // name: "App",
 
   components: {
-    navcomp
+    navcomp,
   },
   data() {
     return {
@@ -88,6 +107,9 @@ export default Vue.extend({
       drawer: false,
       group: null,
       ClassCheckBox: [],
+      alignments: [
+        'center'
+      ],
     };
   },
   watch: {
@@ -120,9 +142,8 @@ export default Vue.extend({
       this.ExoticWeapon =
         jsondata.Exotic.Weapons[getRandomInt(jsondata.Exotic.Weapons.length)];
     },
-    RandomExoticArmor: function (exoticclass: string) {
-      if (exoticclass == "ALL") {
-        const RandomALL = this.ClassCheckBox
+    RandomExoticArmor: function () {
+        const RandomALL = this.ClassCheckBox;
         const RandomClassNumber = getRandomInt(this.ClassCheckBox.length);
         const Class = RandomALL[RandomClassNumber];
 
@@ -148,27 +169,7 @@ export default Vue.extend({
         this.HuntTitWar = Class; //Sets The Class
 
         // console.log(Class);
-      } else if (exoticclass == "Hunter") {
-        this.HuntTitWar = exoticclass;
-        this.ExoticArmor =
-          jsondata.Exotic.Armor.Hunter[
-            getRandomInt(jsondata.Exotic.Armor.Hunter.length)
-          ];
-      } else if (exoticclass == "Titan") {
-        this.HuntTitWar = exoticclass;
-        this.ExoticArmor =
-          jsondata.Exotic.Armor.Titan[
-            getRandomInt(jsondata.Exotic.Armor.Titan.length)
-          ];
-      } else if (exoticclass == "Warlock") {
-        this.HuntTitWar = exoticclass;
-        this.ExoticArmor =
-          jsondata.Exotic.Armor.Warlock[
-            getRandomInt(jsondata.Exotic.Armor.Warlock.length)
-          ];
-      } else {
-        console.log("ERROR");
-      }
+    
     },
   },
 });
--- src/components/nav.vue
+++ src/components/nav.vue
@@ -19,9 +19,9 @@ export default Vue.extend({
 
   data() {
     return {
-      JSONDATA: jsondata,
+      JSONDATA: jsondata
     };
-  },
+  }
 });
 </script>
 
